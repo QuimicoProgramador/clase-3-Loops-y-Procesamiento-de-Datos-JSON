14b6385d7000ac7845918af5f288fd8adb63f47b
QuimicoProgramador <sergio261@gmail.com>
2025-11-14 10:19:07 -0300

    Add forLoop.py to display accepted strings in the Builder

---
+++ forLoop.py
@@ -0,0 +1,4 @@
+cadenas: list = ["Polygon", "Ethereum", "Algorand", "Chainlink","mesa"]
+print("Cadenas admitidas en el Builder:")
+for cadena in cadenas:
+    print(f"- {cadena} está en la lista.")
